3733b29c5d66c1d439c88be866ec222f605ac4bc
joel <joelkalema63@gmail.com>
2022-04-26 04:15:08 +0200

    Added the styles

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,13 @@ import React from "react"
 
 class TodoItem extends React.Component {
   render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+
     return(
       <li>
       <input
@@ -12,7 +19,9 @@ class TodoItem extends React.Component {
       <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
         Delete
       </button>
-      {this.props.todo.title}
+      <span style={this.props.todo.completed ? completedStyle : null}>
+         {this.props.todo.title}
+      </span>
       </li>
     )
   }
