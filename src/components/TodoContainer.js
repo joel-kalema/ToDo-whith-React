dd7b9c5aed4d2d447fc622bf9fe895eaee5ab317
joel <joelkalema63@gmail.com>
2022-04-26 04:00:17 +0200

    Added the add and remove logic

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,36 +1,74 @@
 import React from "react"
 import TodosList from "./TodosList";
-import Header from "./Header"
+import Header from './Header';
+import InputTodo from "./InputTodo"
+import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+  state = {
+    todos: [
+      {
+        id: uuidv4(),
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: uuidv4(),
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: uuidv4(),
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+  };
+  
+  handleChange = id => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo
+      }),
+    }))
+  };
+
+  delTodo = id => {
+    this.setState({
       todos: [
-        {
-          id: 1,
-          title: 'Learn React',
-          completed: false,
-        },
-        {
-          id: 2,
-          title: 'Learn Redux',
-          completed: false,
-        },
-        {
-          id: 3,
-          title: 'Learn React Router',
-          completed: false,
-        },
-      ],
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
+
+  addTodoItem = title => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false
     };
-    this.handleChange = this.handleChange.bind(this);
-  }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <InputTodo addTodoProps={this.addTodoItem} />
+        <TodosList 
+          todos={this.state.todos} 
+          handleChangeProps={this.handleChange} 
+          deleteTodoProps={this.delTodo}/>
       </div>
     )
   }
