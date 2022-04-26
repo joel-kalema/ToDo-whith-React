1ac649b5fdc5f722687eb817915a337ed71cf92a
joel <joelkalema63@gmail.com>
2022-04-26 11:15:21 +0200

    Instaled linters

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,57 +1,56 @@
-import React from "react"
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
 
 class TodoItem extends React.Component {
-  state = {
-    editing: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+    };
   }
 
   handleEditing = () => {
     this.setState({
       editing: true,
-    })
-  }
+    });
+  };
 
   render() {
     const completedStyle = {
-      fontStyle: "italic",
-      color: "#595959",
+      fontStyle: 'italic',
+      color: '#595959',
       opacity: 0.4,
-      textDecoration: "line-through",
+      textDecoration: 'line-through',
+    };
+
+    const viewMode = {};
+    const editMode = {};
+
+    if (this.state.editing) {
+      viewMode.display = 'none';
+    } else {
+      editMode.display = 'none';
     }
 
-    let viewMode = {}
-let editMode = {}
-
-if (this.state.editing) {
-  viewMode.display = "none"
-} else {
-  editMode.display = "none"
-}
-
-    return(
+    return (
       <li>
-        <div div onDoubleClick={this.handleEditing} style={viewMode}>
+
         <input
-        type="checkbox"
-        checked={this.props.todo.completed}
-        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-      /> 
-      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-        Delete
-      </button>
-      <span style={this.props.todo.completed ? completedStyle : null}>
-         {this.props.todo.title}
-      </span>
-        </div>
-        <input
-          type="text"
-          style={editMode}
-          className={styles.textInput}
-          value={title}
-/>
+          type="checkbox"
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        <button type="button" onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        <span style={this.props.todo.completed ? completedStyle : null}>
+          {this.props.todo.title}
+        </span>
+
       </li>
-    )
+    );
   }
 }
 
-export default TodoItem
+export default TodoItem;
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,21 +1,24 @@
-import React from "react"
-import TodoItem from "./TodoItem";
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import TodoItem from './TodoItem';
 
 class TodosList extends React.Component {
   render() {
     return (
       <ul>
-        {this.props.todos.map(todo => (
-         <TodoItem
-         key={todo.id}
-         todo={todo}
-         handleChangeProps={this.props.handleChangeProps}
-         deleteTodoProps={this.props.deleteTodoProps}
-       />
+        {this.props.todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={this.props.handleChangeProps}
+            deleteTodoProps={this.props.deleteTodoProps}
+          />
         ))}
       </ul>
-    )
+    );
   }
 }
 
-export default TodosList
+export default TodosList;
